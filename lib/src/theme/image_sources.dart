fd3890f00ff6c8732c7620491ec305172e316ac6
Ralina <seitovaralina1@gmail.com>
2024-10-30 08:19:35 +0600

    feature: add assets

--- lib/src/theme/image_sources.dart
+++ lib/src/theme/image_sources.dart
@@ -1,3 +1,7 @@
 sealed class ImageSources {
-  static const plusIcon = '';
+  static const placeholder = 'assets/images/placeholder.png';
+  static const blackCoffee = 'assets/images/black_coffee.png';
+  static const coffeeWithMilk = 'assets/images/coffee_with_milk.png';
+  static const tea = 'assets/images/tea.png';
+  static const authorsDrink = 'assets/images/authors_drink.png';
 }
